4936abaa8418f9fc7bed042ade07692226cc8588
root <root@ired.team>
2019-01-17 00:25:48 +0000

    started added some provisioning for http redirector

--- redir-http.tf
+++ redir-http.tf
@@ -5,6 +5,14 @@ resource "digitalocean_droplet" "redir-http" {
   region = "nyc3"
   size   = "s-1vcpu-1gb"
   ssh_keys = ["${digitalocean_ssh_key.dossh.id}"]
+
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get install socat",
+      "socat TCP4-LISTEN:80,fork TCP4:${digitalocean_droplet.c2-http.ipv4_address}:80",
+      "socat TCP4-LISTEN:443,fork TCP4:${digitalocean_droplet.c2-http.ipv4_address}:443",
+    ]
+  }
 }
 
 
