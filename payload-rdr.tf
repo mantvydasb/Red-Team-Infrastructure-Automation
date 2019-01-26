cfcd53a1b3f03790403e3cbf629036a3ceba0f86
root <root@ired.team>
2019-01-26 15:28:32 +0000

    added initial config for proxy mod rewrite

--- payload-rdr.tf
+++ payload-rdr.tf
@@ -8,9 +8,21 @@ resource "digitalocean_droplet" "payload-rdr" {
 
   provisioner "remote-exec" {
     inline = [
-      "export DEBIAN_FRONTEND=noninteractive; apt update && apt-get -y -qq install socat apache2",      
-      "echo \"@reboot root socat TCP4-LISTEN:80,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:80\" >> /etc/cron.d/mdadm",
-      "echo \"@reboot root socat TCP4-LISTEN:443,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:443\" >> /etc/cron.d/mdadm"
+      "export DEBIAN_FRONTEND=noninteractive; apt update && apt-get -y -qq install apache2",      
+      "a2enmod rewrite proxy proxy_http && service apache2 restart"
+      # "echo \"@reboot root socat TCP4-LISTEN:80,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:80\" >> /etc/cron.d/mdadm",
+      # "echo \"@reboot root socat TCP4-LISTEN:443,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:443\" >> /etc/cron.d/mdadm",
+      # "shutdown -r now"
     ]
   }
+  provisioner "file" {
+    source = "./configs/.htaccess"
+    destination = "/var/www/html/.htaccess"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 644 /var/www/html/.htaccess"
+    ]
+  }
+
 }
