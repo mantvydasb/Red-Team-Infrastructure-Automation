0d96784ebda12f861048c7dda266e563a3cf4201
root <root@ired.team>
2019-01-26 16:05:59 +0000

    printing out payload and its redirector IPs

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,9 @@ output "phishing-rdr" {
     "phishing ${cloudflare_record.phishing-a1.name}.${var.domain-c2} - ${digitalocean_droplet.phishing.ipv4_address}", 
     "phishing redirector ${digitalocean_record.phishing-rdr-a0.name}${var.domain-rdir} - ${digitalocean_droplet.phishing-rdr.ipv4_address}", 
     "phishing redirector ${digitalocean_record.phishing-rdr-a1.name}.${var.domain-rdir} - ${digitalocean_droplet.phishing-rdr.ipv4_address}", 
-    "Mail redirector ${digitalocean_record.phishing-rdr-mail-a1.name}.${var.domain-rdir} - ${digitalocean_droplet.phishing-rdr.ipv4_address}",
+    "mail redirector ${digitalocean_record.phishing-rdr-mail-a1.name}.${var.domain-rdir} - ${digitalocean_droplet.phishing-rdr.ipv4_address}",
+    "payload - ${digitalocean_droplet.payload.ipv4_address}",
+    "payload redirector - ${digitalocean_droplet.payload-rdr.ipv4_address}",
     "To finalise the infrastructure, run ./finalize.sh and update the DNS DKIM in DigitalOcean for ${var.domain-rdir} which the script will provide."
     ]
 }
