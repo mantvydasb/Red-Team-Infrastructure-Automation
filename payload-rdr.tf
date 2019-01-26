91bc75fac88c016b2d117f45462e0ab449ac0ae3
root <root@ired.team>
2019-01-26 15:50:56 +0000

    updated htaccess

--- payload-rdr.tf
+++ payload-rdr.tf
@@ -10,19 +10,25 @@ resource "digitalocean_droplet" "payload-rdr" {
     inline = [
       "export DEBIAN_FRONTEND=noninteractive; apt update && apt-get -y -qq install apache2",      
       "a2enmod rewrite proxy proxy_http && service apache2 restart"
-      # "echo \"@reboot root socat TCP4-LISTEN:80,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:80\" >> /etc/cron.d/mdadm",
-      # "echo \"@reboot root socat TCP4-LISTEN:443,fork TCP4:${digitalocean_droplet.payload.ipv4_address}:443\" >> /etc/cron.d/mdadm",
-      # "shutdown -r now"
     ]
   }
   provisioner "file" {
     source = "./configs/.htaccess"
     destination = "/var/www/html/.htaccess"
   }
+  provisioner "file" {
+    source = "./configs/apache2.conf"
+    destination = "/etc/apache2/apache2.conf"
+  }
+
   provisioner "remote-exec" {
     inline = [
       "chmod 644 /var/www/html/.htaccess"
     ]
   }
-
+  provisioner "remote-exec" {
+    inline = [
+      "a2enmod rewrite proxy proxy_http && service apache2 restart"
+    ]
+  }
 }
